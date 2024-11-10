6e4e709a3fd699bf49147b9531536de17f084ba5
Biraj Baral <baralbiraj74@gmail.com>
2024-11-10 17:27:47 +0545

    removed unnecessary

--- Client/src/components/canvas/constant.js
+++
@@ -1,6 +0,0 @@
-export const TOOLS = [
-    { id: 'pencil', icon: 'Pencil', label: 'Pencil' },
-    { id: 'rectangle', icon: 'Square', label: 'Rectangle' },
-    { id: 'circle', icon: 'Circle', label: 'Circle' },
-    { id: 'eraser', icon: 'Eraser', label: 'Eraser' },
-  ];
--- Client/src/store/store.js
+++ Client/src/store/store.js
@@ -1,15 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import profileReducer from "./profileReducer.js";
 import authReducer from "./authSlice.js";
-import toolReducer from "./toolsSlice.js";
-import canvasReducer from "./canvasSlice.js";
-import historyReducer from "./historySlice.js";
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     profile: profileReducer,
-    tools: toolReducer,
-    canvas: canvasReducer,
-    history: historyReducer,
+
   },
 });
