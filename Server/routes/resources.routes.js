8fc5dc2403e53030cd3e417bb5bbf1b1263bf481
Biraj Baral <baralbiraj74@gmail.com>
2024-11-24 14:00:54 +0545

    resources rendered successfully

--- Server/routes/resources.routes.js
+++ Server/routes/resources.routes.js
@@ -1,22 +1,30 @@
 import { Router } from "express";
-import { AddResources, DeleteResource, getResourceByTitle } from "../controllers/resource.controller.js";
+import {
+  AddResources,
+  DeleteResource,
+  getResourceByTitle,
+  getUserUploadedResource,
+  getClassroomResources
+} from "../controllers/resource.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-const router=Router()
+const router = Router();
 
+router.route("/add").post(
+  verifyJwt,
+  upload.fields([
+    {
+      name: "resource",
+      maxCount: 25,
+    },
+  ]),
+  AddResources
+);
 
-router.route('/add').post(
-    verifyJwt,
-    upload.fields([
-        {
-            name:"resource",
-            maxCount:25
-        },
-    ]),
-    AddResources
-)
-
-router.route('/get').get(getResourceByTitle)
-router.route('/delete/:id').delete(verifyJwt,DeleteResource)
-
-export default router
+router.route("/get").get(getResourceByTitle);
+router.route("/delete/:id").delete(verifyJwt, DeleteResource);
+router
+  .route("/getUserUploadedResources")
+  .get(verifyJwt, getUserUploadedResource);
+router.route("/getClassroomResources").get(verifyJwt,getClassroomResources);
+export default router;
