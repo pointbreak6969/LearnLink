4d43ff455bfa3b3a328e3c15794f7f34b8161a7e
Biraj Baral <baralbiraj74@gmail.com>
2024-12-02 19:10:53 +0545

    Remove Redux integration and related files; update package.json and package-lock.json

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./store/store.js";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login";
